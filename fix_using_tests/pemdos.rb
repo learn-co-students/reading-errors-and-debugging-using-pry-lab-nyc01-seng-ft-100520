54c0e503416588c914ee22bd892ed6d6f7b92e68
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-03 16:34:44 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,28 @@
 # don't forget to add: require 'pry'
+require "pry"
+
+# def snake_it_up(string)
+#   if string[0] == "s"
+#     10.times do
+#       print "s"
+#     end
+#       p string
+#       binding.pry
+#   else
+#     p "#{string}"
+#     binding.pry
+#   end
+# end
+
+
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+      return "ssssssssss#{string}"
   else
-    string
-  end
+    return "this did not work"
+    # p "#{string}"
+   end
 end
+
+snake_it_up("surprise!")
