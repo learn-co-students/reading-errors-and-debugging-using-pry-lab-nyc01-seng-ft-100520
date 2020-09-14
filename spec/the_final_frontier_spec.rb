2b7d3d7c5535b4da22bd408af89472e4b2749a66
Anber Jamshed <anberjam@yahoo.com>
2020-09-14 01:11:27 +0000

    Done.

--- spec/the_final_frontier_spec.rb
+++ spec/the_final_frontier_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../fix_using_tests/the_final_frontier'
 
+require_relative '../fix_using_tests/the_final_frontier'
+
 describe "the_final_frontier" do
-  it "`engage` should return the captain's log and star date" do
-    expect(engage).to output("Captain's Log, star date").to_stdout
+  it "`engage` should return the captain's log and crew greetings" do
+    expect{ engage }.to output(/Captain's Log, star date /).to_stdout
   end
 end
